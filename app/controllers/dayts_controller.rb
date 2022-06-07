88dd1d571a5258b23b62d62f7cba8a3d41f17eea
Aren Clissold <arenclissold@gmail.com>
2022-06-07 14:08:57 +1000

    created dayt#new and added users to dayts

--- app/controllers/dayts_controller.rb
+++ app/controllers/dayts_controller.rb
@@ -1,9 +1,29 @@
 class DaytsController < ApplicationController
-  has_many :reviews
 
   def index
   end
 
   def show
+  end
+
+  def new
+    @dayt = Dayt.new
+  end
+
+  def create
+    @dayt = Dayt.new(dayt_params)
+    @dayt.user = current_user
+    if @dayt.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def dayt_params
+    params.require(:dayt).permit(:title, :content, :location, :duration, :price, :opening_time, :closing_time, :booking_url)
   end
 end
--- app/models/dayt.rb
+++ app/models/dayt.rb
@@ -1,2 +1,3 @@
 class Dayt < ApplicationRecord
+  belongs_to :user
 end
