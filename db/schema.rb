a4d7e6dae702a7e81edbf1f44e984e308e884b9c
lea moesch <leamoesch@gmail.com>
2022-06-08 15:29:11 +1000

    navbar_clean

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_06_07_053347) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,10 +64,8 @@ ActiveRecord::Schema.define(version: 2022_06_07_053347) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-
     t.float "latitude"
     t.float "longitude"
-
     t.index ["user_id"], name: "index_dayts_on_user_id"
   end
 
