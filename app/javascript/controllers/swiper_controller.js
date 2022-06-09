cec00def9bc765e6b1bdd57d9b48019a39780ce5
Aren Clissold <arenclissold@gmail.com>
2022-06-09 10:44:08 +1000

    added observer to swiper

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -4,13 +4,13 @@ import Swiper, {EffectCards} from 'swiper';
 export default class extends Controller {
   connect() {
     console.log('Hello, swiper!')
-
-    const swiper = new Swiper(".swiper", {
+    this.swiper = new Swiper(".swiper", {
       modules:[EffectCards],
       effect: "cards",
       cardsEffect: {
         slideShadows: false,
-      }
+      },
+      observer: true
       // preventClicks: false,
       // preventClicksPropagation: false,
       // grabCursor: true
