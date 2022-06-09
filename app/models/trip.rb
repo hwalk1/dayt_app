363b756b4673405a76f3a2b3f8353a386ec10a4d
hayden walker <haydenjwalker1@gmail.com>
2022-06-09 17:49:49 +1000

    destory dependancies

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :user
-  has_many :trip_dayts
+  has_many :trip_dayts , dependent: :destroy
   has_many :dayts, through: :trip_dayts
 
   validates :title, presence: true
