a2a30c88caf40ddf584b26e860b5be068eea2649
Aren Clissold <arenclissold@gmail.com>
2022-06-15 15:34:16 +1000

    added confetti to swiper page and made progress bar larger

--- app/javascript/controllers/trip_dayt_controller.js
+++ app/javascript/controllers/trip_dayt_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "stimulus"
+import confetti from 'canvas-confetti';
 
 export default class extends Controller {
   static targets = ['acceptButton', 'declineButton', 'acceptIcon', 'declineIcon', 'duration']
@@ -70,6 +71,44 @@ export default class extends Controller {
     progressBar.style.width = `${totalProgress}%`
 
     totalProgress >= 70 && progressBar.classList.add('bg-warning')
-    totalProgress >= 100 && progressBar.classList.add('bg-danger')
+
+    if (totalProgress >= 95 && localStorage.getItem("confetti")) this.#confetti()
+  }
+
+  #confetti() {
+    localStorage.setItem("confetti", '')
+    var count = 200;
+    var defaults = {
+      origin: { y: 0.7 }
+    };
+
+    function fire(particleRatio, opts) {
+      confetti(Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio)
+      }));
+    }
+
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    fire(0.2, {
+      spread: 60,
+    });
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
   }
 }
